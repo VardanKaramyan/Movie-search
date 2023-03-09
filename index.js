61b3861711ec2a21a57a7552b6e268541879abe4
VardanKaramyan <33194619+VardanKaramyan@users.noreply.github.com>
2023-03-09 22:19:15 +0400

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@
         return movie.Title;
     },
     async fetchData(searchTerm) {
-        const response = await axios.get('http://www.omdbapi.com/',{
+        const response = await axios.get('https://www.omdbapi.com/',{
             params: {
                 apikey: '51fde68d',
                 s: searchTerm
